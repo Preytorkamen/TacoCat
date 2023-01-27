2ea743619e41cee42b7d6b11001bbf9fd7dbb083
Preytorkamen <101070557+Preytorkamen@users.noreply.github.com>
2023-01-27 16:59:38 -0500

    TacoCat App

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+// Get the string from the page
+//Controller Function
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString, userString);
+}
+
+// Reverse the string
+//Logic Function
+function reverseString(userString){
+    let revString = [];
+
+    //Reverse string using for loop
+    for (let i = userString.length - 1; i >= 0; i--){
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+// Display the reversed string to the user
+function displayString(revString, userString){
+    //Write to the page
+    if (revString.toLowerCase() == userString.toLowerCase()) {
+        document.getElementById("msg").innerHTML = `Your Word IS a Palindrome`;
+    } else {
+        document.getElementById("msg").innerHTML = 'Your Word is NOT a Palindrome';
+    }
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
